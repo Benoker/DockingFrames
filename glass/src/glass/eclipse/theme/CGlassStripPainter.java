dfde9f3e9ee5a74a13e1c596b0913c1b74388250
beni <beni@3428d308-0d2e-0410-b3d4-94d0fefcacdf>
2010-04-11 16:53:32 +0000

    git-svn-id: http://svn.javaforge.com/svn/DockingFrames/trunk@471 3428d308-0d2e-0410-b3d4-94d0fefcacdf

---
+++ glass/src/glass/eclipse/theme/CGlassStripPainter.java
@@ -0,0 +1,159 @@
+/*
+ * Bibliothek - DockingFrames
+ * Library built on Java/Swing, allows the user to "drag and drop"
+ * panels containing any Swing-Component the developer likes to add.
+ * 
+ * Copyright (C) 2007 Benjamin Sigg
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Benjamin Sigg
+ * benjamin_sigg@gmx.ch
+ * CH - Switzerland
+ */
+package glass.eclipse.theme;
+
+import glass.eclipse.theme.utils.*;
+
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+
+import kux.glass.*;
+import bibliothek.extension.gui.dock.theme.eclipse.stack.*;
+import bibliothek.extension.gui.dock.theme.eclipse.stack.tab.*;
+import bibliothek.gui.*;
+import bibliothek.gui.dock.util.color.*;
+
+
+/**
+ * Paints the background of the tab by painting a glass background.
+ * @author Thomas Hilbert
+ */
+@ColorCodes("stack.border.glass")
+public class CGlassStripPainter implements TabPanePainter {
+   private AbstractDockColor color = new AbstractDockColor("stack.border.glass", DockColor.KIND_DOCK_COLOR, Color.BLACK) {
+      @Override
+      protected void changed(Color oldColor, Color newColor) {
+         pane.repaint();
+      }
+   };
+
+   private EclipseTabPane pane;
+
+   IGlassFactory glass = CGlassFactoryGenerator.Create();
+
+   /**
+    * Creates a new painter.
+    * @param tabbedComponent the component for which this painter will work
+    */
+   public CGlassStripPainter(EclipseTabPane pane) {
+      this.pane = pane;
+   }
+
+   public void paint(Graphics g) {
+      paintBackground(g);
+   }
+
+   private void paintHorizontal(Graphics g, Rectangle available, Rectangle bounds, int y) {
+      paintBackground(g, available.x, bounds.y, available.width, bounds.height, true);
+
+      if (available.x < bounds.x - 1) {
+         // left side
+         g.drawLine(available.x, y, bounds.x - 1, y);
+      }
+
+      if (available.x + available.width > bounds.x + bounds.width) {
+         // right side
+         g.drawLine(available.x + available.width, y, bounds.x + bounds.width, y);
+      }
+   }
+
+   private void paintVertical(Graphics g, Rectangle available, Rectangle bounds, int x) {
+      //      paintBackground(g, bounds.x, available.y, bounds.width, available.height, false);
+      paintBackground(g, bounds.x, available.y, available.height, bounds.width, false);
+
+      if (available.y < bounds.y - 1) {
+         g.drawLine(x, available.y, x, bounds.y - 1);
+      }
+      if (available.y + available.height > bounds.y + bounds.height) {
+         g.drawLine(x, available.y + available.height, x, bounds.y + bounds.height);
+      }
+   }
+
+   protected void paintBackground(Graphics g, int x, int y, int w, int h, boolean horizontal) {
+      Graphics2D g2d = (Graphics2D) g.create();
+
+      BufferedImage img = null;
+      try {
+         img = glass.RenderBufferedImage(CGlassEclipseTabPainter.VALUE_PLAIN, new Dimension(w, h), true);
+      } catch (Exception e) {
+         img = glass.RenderBufferedImage(CGlassFactory.VALUE_STEEL, new Dimension(w, h), true);
+      }
+
+      if (!horizontal) {
+         AffineTransform atTrans = AffineTransform.getTranslateInstance(x /*+ h*/, y + w);
+         atTrans.concatenate(COutlineHelper.tRot90CCW);
+
+         g2d.drawImage(img, atTrans, null);
+      } else {
+         g2d.drawImage(img, x, y, null);
+      }
+
+      g2d.dispose();
+   }
+
+   public void setController(DockController controller) {
+      ColorManager colors = controller == null ? null : controller.getColors();
+      color.setManager(colors);
+   }
+
+   public void paintBackground(Graphics g) {
+      Dockable selection = pane.getSelectedDockable();
+      if (selection == null) {
+         return;
+      }
+
+      EclipseTab tab = pane.getTab(selection);
+      if (tab == null || !tab.isPaneVisible()) {
+         return;
+      }
+
+      Rectangle bounds = tab.getBounds();
+      Rectangle available = pane.getAvailableArea();
+
+      g.setColor(color.value());
+
+      switch (pane.getTabPlacement()) {
+         case TOP_OF_DOCKABLE:
+            paintHorizontal(g, available, bounds, bounds.y + bounds.height - 1);
+            break;
+         case BOTTOM_OF_DOCKABLE:
+            paintHorizontal(g, available, bounds, bounds.y);
+            break;
+         case LEFT_OF_DOCKABLE:
+            paintVertical(g, available, bounds, bounds.x + bounds.width - 1);
+            break;
+         case RIGHT_OF_DOCKABLE:
+            paintVertical(g, available, bounds, bounds.x);
+            break;
+      }
+   }
+
+   public void paintForeground(Graphics g) {
+      // TODO Auto-generated method stub
+
+   }
+}
---
+++ glass/src/glass/eclipse/theme/GlassEclipseTabTransmitter.java
@@ -0,0 +1,193 @@
+/*
+ * Bibliothek - DockingFrames
+ * Library built on Java/Swing, allows the user to "drag and drop"
+ * panels containing any Swing-Component the developer likes to add.
+ * 
+ * Copyright (C) 2010 Benjamin Sigg
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Benjamin Sigg
+ * benjamin_sigg@gmx.ch
+ * CH - Switzerland
+ */
+package glass.eclipse.theme;
+
+import java.awt.Color;
+
+import bibliothek.gui.dock.common.intern.color.EclipseTabTransmitter;
+import bibliothek.gui.dock.common.intern.color.TabColorTransmitter;
+import bibliothek.gui.dock.common.intern.theme.color.CColorBridge;
+import bibliothek.gui.dock.themes.ColorBridgeFactory;
+import bibliothek.gui.dock.util.color.ColorManager;
+import bibliothek.util.Colors;
+
+
+/**
+ * A {@link ColorTransmitter} that connects {@link TabColor}s with the
+ * {@link EclipseTheme}.
+ * @author Benjamin Sigg
+ */
+public class GlassEclipseTabTransmitter extends TabColorTransmitter implements CColorBridge{
+    /**
+     * A factory that creates {@link EclipseTabTransmitter}s.
+     */
+    public static final ColorBridgeFactory FACTORY =
+        new ColorBridgeFactory(){
+        public EclipseTabTransmitter create( ColorManager manager ){
+            return new EclipseTabTransmitter( manager );
+        }
+    };
+
+    private static final String KEYS[] = new String[]{
+        "stack.tab.border.glass", 
+        "stack.tab.border.selected.glass", 
+        "stack.tab.border.selected.focused.glass", 
+        "stack.tab.border.selected.focuslost.glass",
+
+        "stack.tab.top.glass",
+        "stack.tab.tob.selected.glass",
+        "stack.tab.top.selected.focused.glass",
+        "stack.tab.top.selected.focuslost.glass",
+
+        "stack.tab.bottom.glass",
+        "stack.tab.bottom.selected.glass",
+        "stack.tab.bottom.selected.focused.glass",
+        "stack.tab.bottom.selected.focuslost.glass",
+
+        "stack.tab.text.glass",
+        "stack.tab.text.selected.glass",
+        "stack.tab.text.selected.focused.glass", 
+        "stack.tab.text.selected.focuslost.glass",
+        
+        "glass.selected.center",
+        "glass.selected.light",
+        "glass.selected.boundary",
+
+        "glass.focused.center",
+        "glass.focused.light",
+        "glass.focused.boundary"
+    };
+
+    public GlassEclipseTabTransmitter( ColorManager manager ){
+        super( manager, KEYS );
+    }
+    
+    public boolean matches( String id ){
+	    return id.startsWith( "glass." ) || id.endsWith( ".glass" );
+    }
+
+    @Override
+    protected boolean isFocused( String id ) {
+        return id.contains( "focused" ) || id.contains( "focuslost" );
+    }
+
+    @Override
+    protected boolean isSelected( String id ) {
+        return id.contains( "selected" );
+    }
+
+    @Override
+    protected boolean isForeground( String id ) {
+        return id.contains( "text" );
+    }
+
+    @Override
+    protected Color convert( Color source, String key ) {
+        if( isSelected( key ))
+            return convertSelected( source, key );
+
+        if( isFocused( key ))
+            return convertFocused( source, key );
+
+        if( "stack.tab.border.glass".equals( key ))
+            return source;
+
+        if(  "stack.tab.tob.glass".equals( key ))
+            return Colors.undiffMirror( source, 0.5 );
+
+        if( "stack.tab.bottom.glass".equals( key ))
+            return source;
+
+        if( "stack.tab.text.glass".equals( key ))
+            return Colors.diffMirror( source, 1.0 );
+
+        return null;
+    }
+
+    @Override
+    protected Color convertSelected( Color source, String key ) {
+        if( isFocused( key ))
+            return convertFocused( source, key );
+
+        if( "stack.tab.border.selected.glass".equals( key ))
+            return source;
+
+        if(  "stack.tab.tob.selected.glass".equals( key ))
+            return Colors.undiffMirror( source, 0.5 );
+
+        if( "stack.tab.bottom.selected.glass".equals( key ))
+            return source;
+
+        if( "stack.tab.text.selected.glass".equals( key ))
+            return Colors.diffMirror( source, 1.0 );
+
+        if( "glass.selected.center".equals( key ))
+        	return Colors.brighter( source, 0.25 );
+        	
+        if( "glass.selected.light".equals( key ))
+        	return Colors.brighter( source, 0.5 );
+        	
+        if( "glass.selected.boundary".equals( key ))
+        	return Colors.darker( source, 0.25 );
+
+        
+        return null;
+    }
+
+    @Override
+    protected Color convertFocused( Color source, String key ) {
+        if( "stack.tab.border.selected.focused.glass".equals( key ))
+            return source;
+        if( "stack.tab.border.selected.focuslost.glass".equals( key ))
+            return source;
+
+        if( "stack.tab.top.selected.focused.glass".equals( key ))
+            return Colors.undiffMirror( source, 0.5 );
+        if( "stack.tab.top.selected.focuslost.glass".equals( key ))
+            return Colors.undiffMirror( source, 0.5 );
+
+        if( "stack.tab.bottom.selected.focused.glass".equals( key ))
+            return source;
+        if( "stack.tab.bottom.selected.focuslost.glass".equals( key ))
+            return source;
+
+        if( "stack.tab.text.selected.focused.glass".equals( key ))
+            return Colors.diffMirror( source, 1.0 ); 
+        if( "stack.tab.text.selected.focuslost.glass".equals( key ))
+            return Colors.diffMirror( source, 1.0 );
+
+        if( "glass.focused.center".equals( key ))
+        	return source;
+        	
+        if( "glass.focused.light".equals( key ))
+        	return Colors.brighter( source, 0.5 );
+        	
+        if( "glass.focused.boundary".equals( key ))
+        	return Colors.darker( source, 0.5 );
+        
+        return null;
+    }
+}
