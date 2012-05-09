bb03eb8e750b50162959ae23ae654337b7a1b38e
Herve Guillaume <rvguillaume@hotmail.com>
2012-05-09 15:31:12 +0200

    Add gradient to SlimScroolbar

--- docking-frames-ext-toolbar/src/bibliothek/gui/dock/station/toolbar/group/SlimScrollbar.java
+++ docking-frames-ext-toolbar/src/bibliothek/gui/dock/station/toolbar/group/SlimScrollbar.java
@@ -33,11 +33,15 @@ import java.awt.Adjustable;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.GradientPaint;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.event.AdjustmentEvent;
 import java.awt.event.AdjustmentListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,17 +57,19 @@ import bibliothek.gui.dock.ToolbarGroupDockStation;
 
 /**
  * A slim version of a {@link JScrollBar}.
+ * 
  * @author Benjamin Sigg
  */
-public class SlimScrollbar extends JComponent implements ColumnScrollBar, Adjustable{
+public class SlimScrollbar extends JComponent implements ColumnScrollBar,
+		Adjustable{
 	private BoundedRangeModel model;
 	private Orientation orientation = Orientation.HORIZONTAL;
 	private List<AdjustmentListener> listeners = new ArrayList<AdjustmentListener>();
-	
+
 	private boolean hover = false;
 	private boolean mousePressed = false;
 	private float mouseOffset = 0;
-	
+
 	/**
 	 * A factory creating new {@link SlimScrollbar}s.
 	 */
@@ -73,156 +79,172 @@ public class SlimScrollbar extends JComponent implements ColumnScrollBar, Adjust
 			return new SlimScrollbar();
 		}
 	};
-	
+
 	/**
 	 * Creates a new scrollbar
 	 */
 	public SlimScrollbar(){
-		model = new DefaultBoundedRangeModel( 0, 0, 0, 1 );
-		model.addChangeListener( new ChangeListener(){
+		model = new DefaultBoundedRangeModel(0, 0, 0, 1);
+		model.addChangeListener(new ChangeListener(){
 			@Override
 			public void stateChanged( ChangeEvent e ){
-				AdjustmentEvent event = new AdjustmentEvent( SlimScrollbar.this, 0, 0, getValue() );
-				for( AdjustmentListener listener : listeners ){
-					listener.adjustmentValueChanged( event );
+				AdjustmentEvent event = new AdjustmentEvent(SlimScrollbar.this,
+						0, 0, getValue());
+				for (AdjustmentListener listener : listeners){
+					listener.adjustmentValueChanged(event);
 				}
 			}
-		} );
+		});
 		MouseAdapter adapter = new MouseAdapter(){
 			@Override
 			public void mousePressed( MouseEvent e ){
-				onMouseEvent( e, true );
+				onMouseEvent(e, true);
 			}
+
 			@Override
 			public void mouseReleased( MouseEvent e ){
-				mousePressed = (e.getModifiersEx() & (MouseEvent.BUTTON1_DOWN_MASK | MouseEvent.BUTTON2_DOWN_MASK | MouseEvent.BUTTON3_DOWN_MASK)) != 0;
-				onMouseEvent( e, false );
+				mousePressed = (e.getModifiersEx() & (MouseEvent.BUTTON1_DOWN_MASK
+						| MouseEvent.BUTTON2_DOWN_MASK | MouseEvent.BUTTON3_DOWN_MASK)) != 0;
+				onMouseEvent(e, false);
 			}
+
 			@Override
 			public void mouseDragged( MouseEvent e ){
-				onMouseEvent( e, true );
+				onMouseEvent(e, true);
 			}
+
 			@Override
 			public void mouseEntered( MouseEvent e ){
-				onMouseEvent( e, false );
+				onMouseEvent(e, false);
 			}
+
 			@Override
 			public void mouseExited( MouseEvent e ){
 				hover = false;
 				repaint();
 			}
+
 			@Override
 			public void mouseMoved( MouseEvent e ){
-				onMouseEvent( e, false );
+				onMouseEvent(e, false);
 			}
 		};
-		addMouseListener( adapter );
-		addMouseMotionListener( adapter );
+		addMouseListener(adapter);
+		addMouseMotionListener(adapter);
 	}
-	
+
 	private void onMouseEvent( MouseEvent event, boolean mouseDown ){
 		int value = model.getValue();
 		int thumb = getThumbSize();
 		float track = getTrack();
 		int position;
-		if( orientation == Orientation.HORIZONTAL ){
+		if (orientation == Orientation.HORIZONTAL){
 			position = event.getX();
-		}
-		else{
+		} else{
 			position = event.getY();
 		}
-		
-		if( track > 0 ){
+
+		if (track > 0){
 			float range = model.getMaximum() - model.getMinimum();
 			float factor = range / track;
-			
-			if( mouseDown && !mousePressed ){
+
+			if (mouseDown && !mousePressed){
 				mouseOffset = position - getThumbStart();
-				if( mouseOffset < 0 || mouseOffset >= thumb ){
-					mouseOffset = thumb/2;
+				if (mouseOffset < 0 || mouseOffset >= thumb){
+					mouseOffset = thumb / 2;
 				}
 				mousePressed = true;
 			}
-			if( mouseDown && mousePressed ){
-				setValue( (int)( (position - mouseOffset) * factor ) );
+			if (mouseDown && mousePressed){
+				setValue((int) ((position - mouseOffset) * factor));
 			}
 		}
-		
-		if( mouseDown ){
+
+		if (mouseDown){
 			hover = true;
-		}
-		else if( contains( event.getPoint() )){
+		} else if (contains(event.getPoint())){
 			hover = value <= position && position <= value + thumb;
-		}
-		else{
+		} else{
 			hover = false;
 		}
 		repaint();
 	}
-	
+
 	@Override
 	public Dimension getMinimumSize(){
-		return new Dimension( 6, 6 );
+		return new Dimension(6, 6);
 	}
-	
+
 	@Override
 	public Dimension getPreferredSize(){
-		return new Dimension( 6, 6 );
+		return new Dimension(6, 6);
 	}
-	
+
 	@Override
 	protected void paintComponent( Graphics g ){
-		super.paintComponent( g );
-		if( hover ){
-			g.setColor( Color.BLACK );
-		}
-		else{
-			g.setColor( Color.DARK_GRAY );
-		}
-		
+		super.paintComponent(g);
+		Graphics2D g2D = (Graphics2D) g;
+
 		int value = getThumbStart();
 		int thumb = getThumbSize();
-		
-		if( orientation == Orientation.HORIZONTAL ){
-			g.fillRect( value, 0, thumb, 6 );
+
+		Rectangle2D rec = null;
+		if (hover){
+			if (orientation == Orientation.HORIZONTAL){
+				g2D.setPaint(new GradientPaint(new Point(value, 0),
+						new Color(130, 130, 130), new Point(value, 6), new Color(51, 51, 51)));
+				rec = new Rectangle2D.Double(value, 0, thumb, 6);
+			} else{
+				g2D.setPaint(new GradientPaint(new Point(0, value),
+						new Color(130, 130, 130), new Point(6, value), new Color(51, 51, 51)));
+				rec = new Rectangle2D.Double(0, value, 6, thumb);
+			}
+		} else{
+			if (orientation == Orientation.HORIZONTAL){
+				g2D.setPaint(new GradientPaint(new Point(value, 0),
+						new Color(100, 100, 100), new Point(value, 6), new Color(10, 10, 10)));
+				rec = new Rectangle2D.Double(value, 0, thumb, 6);
+			} else{
+				g2D.setPaint(new GradientPaint(new Point(0, value),
+						new Color(100, 100, 100), new Point(6, value), new Color(10, 10, 10)));
+				rec = new Rectangle2D.Double(0, value, 6, thumb);
+			}
+
 		}
-		else{
-			g.fillRect( 0, value, 6, thumb );
-		}
+		g2D.fill(rec);
 	}
-	
+
 	private int getThumbStart(){
 		float range = model.getMaximum() - model.getMinimum();
 		float track = getTrack();
-		
-		return (int)(model.getValue() * (track / range));
+
+		return (int) (model.getValue() * (track / range));
 	}
-	
+
 	private int getThumbSize(){
 		float range = model.getMaximum() - model.getMinimum();
 		float extent = model.getExtent();
-		
+
 		float track = getTrack();
-		
-		int thumb = (int)(track * (extent / range));
-		thumb = Math.max( thumb, 20 );
-		thumb = Math.min( thumb, (int)track );
+
+		int thumb = (int) (track * (extent / range));
+		thumb = Math.max(thumb, 20);
+		thumb = Math.min(thumb, (int) track);
 		return thumb;
 	}
-	
+
 	private int getTrack(){
-		if( orientation == Orientation.HORIZONTAL ){
+		if (orientation == Orientation.HORIZONTAL){
 			return getWidth();
-		}
-		else{
+		} else{
 			return getHeight();
 		}
 	}
-	
+
 	@Override
 	public void setValues( int required, int available ){
-		model.setMaximum( required );
-		model.setExtent( available );
+		model.setMaximum(required);
+		model.setExtent(available);
 	}
 
 	@Override
@@ -243,27 +265,26 @@ public class SlimScrollbar extends JComponent implements ColumnScrollBar, Adjust
 
 	@Override
 	public void addAdjustmentListener( AdjustmentListener listener ){
-		listeners.add( listener );
+		listeners.add(listener);
 	}
 
 	@Override
 	public void removeAdjustmentListener( AdjustmentListener listener ){
-		listeners.remove( listener );
+		listeners.remove(listener);
 	}
 
 	@Override
 	public int getOrientation(){
-		if( orientation == Orientation.HORIZONTAL ){
+		if (orientation == Orientation.HORIZONTAL){
 			return HORIZONTAL;
-		}
-		else{
+		} else{
 			return VERTICAL;
 		}
 	}
 
 	@Override
 	public void setMinimum( int min ){
-		model.setMinimum( min );
+		model.setMinimum(min);
 	}
 
 	@Override
@@ -273,7 +294,7 @@ public class SlimScrollbar extends JComponent implements ColumnScrollBar, Adjust
 
 	@Override
 	public void setMaximum( int max ){
-		model.setMaximum( max );
+		model.setMaximum(max);
 	}
 
 	@Override
@@ -316,6 +337,6 @@ public class SlimScrollbar extends JComponent implements ColumnScrollBar, Adjust
 
 	@Override
 	public void setValue( int v ){
-		model.setValue( v );
+		model.setValue(v);
 	}
 }
