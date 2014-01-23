a2ce5ba6937e5c2cd39393d297bcb77ab36a5ac7
Benjamin Sigg <benjamin_sigg@gmx.ch>
2014-01-23 19:27:08 +0100

    Fixed the RectGradientPainter again, the line at the left or top side should be painted for the first tab if it is at least 2 pixels away from the border (if it is only one pixel away the line strangely merges with the border).

--- docking-frames-core/src/bibliothek/extension/gui/dock/theme/eclipse/stack/tab/RectGradientPainter.java
+++ docking-frames-core/src/bibliothek/extension/gui/dock/theme/eclipse/stack/tab/RectGradientPainter.java
@@ -354,7 +354,7 @@ public class RectGradientPainter extends BaseTabComponent {
 				g2d.drawLine( 0, 1, 0, height );
 				g2d.setPaint( normalBackground );
 				// left
-				if (tabIndex != 0 || getX() >= 1) {
+				if (tabIndex != 0 || getX() > 1) {
 					g2d.drawLine(1, 0, 1, 0);
 					g2d.drawLine(0, 1, 0, height);
 				}
@@ -369,7 +369,7 @@ public class RectGradientPainter extends BaseTabComponent {
 				g2d.drawLine( 0, height-2, 0, 0 );
 				g2d.setPaint( normalBackground );
 				// left
-				if (tabIndex != 0 || getX() >= 1) {
+				if (tabIndex != 0 || getX() > 1) {
 					g2d.drawLine(1, height-1, 1, height-1 );
 					g2d.drawLine(0, height-2, 0, 0 );
 				}
@@ -384,7 +384,7 @@ public class RectGradientPainter extends BaseTabComponent {
 				g2d.drawLine( 1, 0, width, 0 );
 				g2d.setPaint( normalBackground );
 				// left
-				if (tabIndex != 0 || getY() >= 1) {
+				if (tabIndex != 0 || getY() > 1) {
 					g2d.drawLine( 0, 1, 0, 1 );
 					g2d.drawLine( 1, 0, width, 0 );
 				}
@@ -399,7 +399,7 @@ public class RectGradientPainter extends BaseTabComponent {
 				g2d.drawLine( 0, 0, width-1, 0 );
 				g2d.setPaint( normalBackground );
 				// left
-				if (tabIndex != 0 || getY() >= 1) {
+				if (tabIndex != 0 || getY() > 1) {
 					g2d.drawLine( width-1, 1, width-1, 1 );
 					g2d.drawLine( 0, 0, width-2, 0 );
 				}
