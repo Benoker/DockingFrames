ffd72a906baff95c4d02dd46c85a971348ce4102
Benjamin Sigg <benjamin_sigg@gmx.ch>
2015-05-03 09:01:33 +0200

    Bugfix: Glass-EclipseThemeExtension will no longer register a title as "set by the client", instead is register a title as "set by the theme". This will allow the framework to remove the title if the theme is switched.

--- docking-frames-ext-glass/src/glass/eclipse/theme/EclipseThemeExtension.java
+++ docking-frames-ext-glass/src/glass/eclipse/theme/EclipseThemeExtension.java
@@ -12,68 +12,70 @@ import bibliothek.gui.dock.util.property.*;
 import glass.eclipse.theme.factory.*;
 import glass.eclipse.theme.icon.*;
 
-
 public class EclipseThemeExtension implements DockThemeExtension {
-   private DockTheme trigger;
+	private DockTheme trigger;
 
-   /**
-    *  Tells the glass eclipse painter which glass parameters should be used to render the glass effect.
-    *  @see CDefaultGlassFactory
-    */
-   public static final PropertyKey<IGlassParameterFactory> GLASS_FACTORY = new PropertyKey<IGlassParameterFactory>("Glass eclipse glass parameter factory", new ConstantPropertyFactory<IGlassParameterFactory>(new CDefaultGlassFactory()), true);
+	/**
+	 *  Tells the glass eclipse painter which glass parameters should be used to render the glass effect.
+	 *  @see CDefaultGlassFactory
+	 */
+	public static final PropertyKey<IGlassParameterFactory> GLASS_FACTORY = new PropertyKey<IGlassParameterFactory>( "Glass eclipse glass parameter factory",
+			new ConstantPropertyFactory<IGlassParameterFactory>( new CDefaultGlassFactory() ), true );
 
-   public EclipseThemeExtension (DockTheme trigger, EclipseTheme theme) {
-      this.trigger = trigger;
+	public EclipseThemeExtension( DockTheme trigger, EclipseTheme theme ) {
+		this.trigger = trigger;
 
-      theme.setMovingImageFactory(new CMiniPreviewMovingImageFactory(128), Priority.THEME);
-      theme.setPaint(new CGlassStationPaint(), Priority.THEME);
+		theme.setMovingImageFactory( new CMiniPreviewMovingImageFactory( 128 ), Priority.THEME );
+		theme.setPaint( new CGlassStationPaint(), Priority.THEME );
 
-   }
+	}
 
-   public void install (DockController controller, DockTheme theme) {
-      DockTitleManager manager = controller.getDockTitleManager();
-      manager.registerClient( FlapDockStation.BUTTON_TITLE_ID, CGlassDockTitleFactory.FACTORY );
-   }
+	public void install( DockController controller, DockTheme theme ) {
+		// nothing to do
+	}
 
-   public void installed (DockController controller, DockTheme theme) {
-      if (trigger == theme) {
-         IconManager im = controller.getIcons();
+	public void installed( DockController controller, DockTheme theme ) {
+		if( trigger == theme ) {
+			DockTitleManager manager = controller.getDockTitleManager();
+			manager.registerTheme( FlapDockStation.BUTTON_TITLE_ID, CGlassDockTitleFactory.FACTORY );
 
-         Icon normalizeIcon = createIcon("images/normalize.png");
-         Icon maximizeIcon = createIcon("images/maximize.png");
+			IconManager im = controller.getIcons();
 
-         im.setIconTheme("split.normalize", normalizeIcon);
-         im.setIconTheme("split.maximize", maximizeIcon);
+			Icon normalizeIcon = createIcon( "images/normalize.png" );
+			Icon maximizeIcon = createIcon( "images/maximize.png" );
 
-         im.setIconTheme("screen.normalize", normalizeIcon);
-         im.setIconTheme("screen.maximize", maximizeIcon);
+			im.setIconTheme( "split.normalize", normalizeIcon );
+			im.setIconTheme( "split.maximize", maximizeIcon );
 
-         im.setIconTheme("locationmanager.maximize", maximizeIcon);
-         im.setIconTheme("locationmanager.normalize", normalizeIcon);
-         im.setIconTheme("locationmanager.externalize", createIcon("images/externalize.png"));
-         im.setIconTheme("locationmanager.minimize", createIcon("images/minimize.png"));
+			im.setIconTheme( "screen.normalize", normalizeIcon );
+			im.setIconTheme( "screen.maximize", maximizeIcon );
 
-         im.setIconTheme("locationmanager.unexternalize", createIcon("images/unexternalize.png"));
-         im.setIconTheme("locationmanager.unmaximize_externalized", normalizeIcon);
+			im.setIconTheme( "locationmanager.maximize", maximizeIcon );
+			im.setIconTheme( "locationmanager.normalize", normalizeIcon );
+			im.setIconTheme( "locationmanager.externalize", createIcon( "images/externalize.png" ) );
+			im.setIconTheme( "locationmanager.minimize", createIcon( "images/minimize.png" ) );
 
-         im.setIconTheme("close", createIcon("images/close_active.png"));
-         im.setIconTheme("flap.hold", createIcon("images/pin_active.png"));
-         im.setIconTheme("flap.free", createIcon("images/unpin_active.png"));
-         im.setIconTheme("overflow.menu", createIcon("images/overflow_menu.png"));
+			im.setIconTheme( "locationmanager.unexternalize", createIcon( "images/unexternalize.png" ) );
+			im.setIconTheme( "locationmanager.unmaximize_externalized", normalizeIcon );
 
-         im.publish(Priority.CLIENT, TabMenuDockIcon.KIND_TAB_MENU, new CTabMenuOverflowIconBridge());
-      }
-   }
+			im.setIconTheme( "close", createIcon( "images/close_active.png" ) );
+			im.setIconTheme( "flap.hold", createIcon( "images/pin_active.png" ) );
+			im.setIconTheme( "flap.free", createIcon( "images/unpin_active.png" ) );
+			im.setIconTheme( "overflow.menu", createIcon( "images/overflow_menu.png" ) );
 
-   public void uninstall (DockController controller, DockTheme theme) {
-      if (trigger == theme) {
-         IconManager im = controller.getIcons();
+			im.publish( Priority.CLIENT, TabMenuDockIcon.KIND_TAB_MENU, new CTabMenuOverflowIconBridge() );
+		}
+	}
 
-         im.clear(Priority.THEME);
-      }
-   }
+	public void uninstall( DockController controller, DockTheme theme ) {
+		if( trigger == theme ) {
+			IconManager im = controller.getIcons();
 
-   public static ImageIcon createIcon (String path) {
-      return new ImageIcon(CGlassEclipseTabPainter.class.getResource(path));
-   }
+			im.clear( Priority.THEME );
+		}
+	}
+
+	public static ImageIcon createIcon( String path ) {
+		return new ImageIcon( CGlassEclipseTabPainter.class.getResource( path ) );
+	}
 }
