af027e824a5951dccc89d81322f0db9fc5b8cf90
Benjamin Sigg <benjamin_sigg@gmx.ch>
2017-02-26 09:47:10 +0100

    Bugfix: An exception was thrown when a CDockable was externalized, the eclipse-theme was selected, and someone was calling CControl.destroy

--- docking-frames-core/src/bibliothek/gui/dock/themes/basic/TabDecorator.java
+++ docking-frames-core/src/bibliothek/gui/dock/themes/basic/TabDecorator.java
@@ -269,7 +269,7 @@ public class TabDecorator implements BasicDockableDisplayerDecorator, StackDockC
 	}
 
 	public DockElementRepresentative getMoveableElement(){
-		if( component.getTabCount() > 0 ){
+		if( component != null && component.getTabCount() > 0 ){
 			return component.getTabAt( 0 );
 		}
 		return null;
